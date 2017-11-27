27c6319e1e81538f15c11b2a946f0f43414d15ad
reihudec <33289659+reihudec@users.noreply.github.com>
2017-11-27 21:09:07 +0300

    Add files via upload

--- js/script_7.js
+++ js/script_7.js
@@ -2,6 +2,7 @@
 
 (function ($) {
     $(document).ready(function () {
+        //Занятие 7 (слайдер)
         $(".lp-slider3").owlCarousel({
             items: 1
         });
@@ -10,20 +11,29 @@
         });
 
         $('.owl-item').on('click', function () {
-            $(this).addClass('clicked');
-            var indexImage = $('.lp-slider4 div.owl-item').index($('.clicked'));
-            $('.lp-slider3').trigger('to.owl.carousel', indexImage);
-            if ($('.lp-slider4 div.active.owl-item.active').index($('.clicked')) == 2) {
-
-                $(this).trigger('next.owl.carousel');
-                console.log($('.lp-slider4 div.owl-item.active').index($('.clicked')));
-                $(this).removeClass('clicked');
-            } else if ($('.lp-slider4 div.owl-item.active').index($('.clicked')) == 0) {
-                $(this).trigger('prev.owl.carousel');
-                console.log($('.lp-slider4 div.owl-item.active').index($('.clicked')));
-                $(this).removeClass('clicked');
-            } else {
-                $(this).removeClass('clicked');
+            var slider3Length = $('.lp-slider3 div.owl-item').length;
+            var slider4Length = $('.lp-slider4 div.owl-item').length;
+            if (slider3Length == slider4Length) {
+                $(this).addClass('clicked');
+                var slides = $('.lp-slider4 div.active.owl-item').length;
+                if (slides == 4) {
+                    slides = slides - 2;
+                } else slides = slides - 1;
+                console.log(slides);
+                var indexImage = $('.lp-slider4 div.owl-item').index($('.clicked'));
+                $('.lp-slider3').trigger('to.owl.carousel', indexImage);
+                if ($('.lp-slider4 div.active.owl-item.active').index($('.clicked')) == slides) {
+                    console.log($('.lp-slider4 div.active.owl-item.active').index($('.clicked')));
+                    $(this).trigger('next.owl.carousel');
+                    $(this).removeClass('clicked');
+                } else if ($('.lp-slider4 div.owl-item.active').index($('.clicked')) == 0) {
+                    console.log($('.lp-slider4 div.active.owl-item.active').index($('.clicked')));
+                    $(this).trigger('prev.owl.carousel');
+                    $(this).removeClass('clicked');
+                } else {
+                    console.log($('.lp-slider4 div.active.owl-item.active').index($('.clicked')));
+                    $(this).removeClass('clicked');
+                }
             }
         });
     });
